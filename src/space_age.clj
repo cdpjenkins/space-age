73aed01cdde4af7501e330fa614b770421cedc4c
Chris Jenkins <chris.jenkins@masabi.com>
2020-11-07 11:52:31 +0000

    .gitignore

--- src/space_age.clj
+++ src/space_age.clj
@@ -3,16 +3,25 @@
 (defn- seconds-to-earth-years [seconds]
   (/ seconds 31557600.0))
 
-(defn- age-fn [year-ratio]
-  (fn [seconds]
-    (let [earth-years (seconds-to-earth-years seconds)]
-      (/ earth-years year-ratio))))
+(def earth-years {:mercury 0.2408467
+                  :venus   0.61519726
+                  :earth   1.0
+                  :mars    1.8808158
+                  :jupiter 11.862615
+                  :saturn  29.447498
+                  :uranus  84.016846
+                  :neptune 164.79132})
 
-(def on-mercury (age-fn 0.240846))
-(def on-venus   (age-fn 0.6151972))
-(def on-earth   (age-fn 1))
-(def on-mars    (age-fn 1.8808158))
-(def on-jupiter (age-fn 11.862615))
-(def on-saturn  (age-fn 29.447498))
-(def on-neptune (age-fn 164.79132))
-(def on-uranus  (age-fn 84.016846))
+(defn- age-fn [planet]
+  (let [year-ratio (earth-years planet)]
+    (fn [seconds]
+      (/ (seconds-to-earth-years seconds) year-ratio))))
+
+(def on-mercury (age-fn :mercury))
+(def on-venus   (age-fn :venus))
+(def on-earth   (age-fn :earth))
+(def on-mars    (age-fn :mars))
+(def on-jupiter (age-fn :jupiter))
+(def on-saturn  (age-fn :saturn))
+(def on-neptune (age-fn :neptune))
+(def on-uranus  (age-fn :uranus))
